686e0ca87643e51dd9820d01422349eee5630cbf
Danny Grove <danny@drgrovellc.com>
2014-04-12 02:50:55 -0400

    Base Yo-Angular App

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('packManApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
